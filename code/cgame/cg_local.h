f1a5733469b8357e3508f56a3d32c6fd9326441d
wolfetplayer <karabastov@yahoo.com>
2021-09-04 14:16:25 +0300

    Separate explosion sound for the Barrel

--- code/cgame/cg_local.h
+++ code/cgame/cg_local.h
@@ -1326,7 +1326,8 @@ typedef struct {
 	sfxHandle_t sfx_grenexp;
 	sfxHandle_t sfx_grenexpDist;
 	sfxHandle_t sfx_dynamiteexp;
-	sfxHandle_t sfx_dynamiteexpDist;   
+	sfxHandle_t sfx_dynamiteexpDist;  
+	sfxHandle_t sfx_barrelexp; 
 	sfxHandle_t sfx_knifehit[5];
 	sfxHandle_t sfx_bullet_metalhit[3];
 	sfxHandle_t sfx_bullet_woodhit[3];

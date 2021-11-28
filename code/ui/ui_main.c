f6488b553385a542614604a63b0fcfba54fc9a84
German <karabastov@yahoo.com>
2021-11-28 14:22:29 +0300

    Gamefinished leads to Credits

--- code/ui/ui_main.c
+++ code/ui/ui_main.c
@@ -7194,7 +7194,7 @@ void _UI_SetActiveMenu( uiMenuCommand_t menu ) {
 			trap_Cvar_Set( "g_reloading", "0" );
 
 			trap_Key_SetCatcher( KEYCATCH_UI );
-			Menus_ActivateByName( "main" );
+			Menus_ActivateByName( "credit_menu" );
 			return;
 //----(SA)	end
 

28c8a4f0853e2dd0dade647876ec3caeb036f139
German <karabastov@yahoo.com>
2024-11-02 17:12:01 +0300

    invisible_user can use price now

--- code/game/g_mover.c
+++ code/game/g_mover.c
@@ -4677,6 +4677,20 @@ they /don't/ need to be all uppercase
 
 void use_invisible_user( gentity_t *ent, gentity_t *other, gentity_t *activator ) {
 	gentity_t *player;
+	int price;
+
+	price = ent->price;
+
+	if ( !price ) {
+        price = 0;
+    }
+
+
+	// Check if player has enough points
+    if (activator->client->ps.persistant[PERS_SCORE] < price) {
+        trap_SendServerCommand( -1, "mu_play sound/items/use_nothing.wav 0\n" );
+        return;  // Player doesn't have enough points
+    }
 
 	if ( ent->wait < level.time ) {
 		ent->wait = level.time + ent->delay;
@@ -4725,6 +4739,8 @@ void use_invisible_user( gentity_t *ent, gentity_t *other, gentity_t *activator 
 
 	G_UseTargets( ent, other ); //----(SA)	how about this so the triggered targets have an 'activator' as well as an 'other'?
 								//----(SA)	Please let me know if you forsee any problems with this.
+
+	activator->client->ps.persistant[PERS_SCORE] -= price;
 }
 
 

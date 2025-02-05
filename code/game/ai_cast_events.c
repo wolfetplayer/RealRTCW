c187f5e5d6f593d1b59d646c92ab27ed31f4cc4e
German <karabastov@yahoo.com>
2025-02-05 20:42:17 +0300

    Friendlies have separate respawn time

--- code/game/ai_cast_events.c
+++ code/game/ai_cast_events.c
@@ -613,8 +613,14 @@ void AICast_Die( gentity_t *self, gentity_t *inflictor, gentity_t *attacker, int
                if (rebirthTime < svParams.minSpawnTime * 1000) {
                  rebirthTime = svParams.minSpawnTime * 1000;
                }
+               
+			   // Friendlies has separate time. Might outsource it.
+			   if (self->aiTeam == 1) {
+                  cs->rebirthTime = level.time + 60000 + rand() % 2000;
+			   } else {
+				   cs->rebirthTime = level.time + rebirthTime + rand() % 2000;
+			   }
 
-                cs->rebirthTime = level.time + rebirthTime + rand() % 2000;
            } else if ( g_gameskill.integer == GSKILL_EASY ) {
 				cs->rebirthTime = level.time + 25000 + rand() % 2000;
 			} else if ( g_gameskill.integer == GSKILL_MEDIUM ) {

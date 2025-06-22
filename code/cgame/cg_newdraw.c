20bb4c85134c81d76df5d2f5457df8e9246ede9b
German <karabastov@yahoo.com>
2025-06-22 17:09:21 +0300

    Always show recharge bar in Survival

--- code/cgame/cg_newdraw.c
+++ code/cgame/cg_newdraw.c
@@ -2069,13 +2069,25 @@ static void CG_DrawWeapRecharge( rectDef_t *rect, vec4_t color, int align ) {
 
 		weap = cg.snap->ps.weapon;
 
-		if ( !( cg.snap->ps.eFlags & EF_ZOOMING ) ) {
-			if ( weap != WP_AIRSTRIKE && weap !=WP_POISONGAS_MEDIC && weap !=WP_DYNAMITE_ENG ) {
-				//fade = qtrue;
+		// Always show in Survival mode
+		if (cgs.gametype == GT_SURVIVAL)
+		{
+			// proceed normally
+		}
+		// In Single Player, show only if using WP_AIRSTRIKE
+		else if (cgs.gametype == GT_SINGLE_PLAYER)
+		{
+			if (weap != WP_AIRSTRIKE)
+			{
 				return;
 			}
 		}
-
+		// In all other modes, hide
+		else
+		{
+			return;
+		}
+		
 		// Determine charge time based on class
 		switch (cg.snap->ps.stats[STAT_PLAYER_CLASS])
 		{

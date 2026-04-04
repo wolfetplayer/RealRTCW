e163427b62d152010a6c82d42ccb00232639e4af
German <karabastov@yahoo.com>
2026-04-05 01:25:19 +0300

    weapon wheel - draw grenades too

--- code/cgame/cg_weapons.c
+++ code/cgame/cg_weapons.c
@@ -5317,6 +5317,14 @@ void CG_UpdateWeaponWheelSelection( float cursorx, float cursory ) {
 
 		case WP_VENOM:
 		case WP_TESLA:
+
+		case WP_GRENADE_LAUNCHER:
+		case WP_POISONGAS:
+		case WP_GRENADE_PINEAPPLE:
+		case WP_DYNAMITE:
+		case WP_DYNAMITE_ENG:
+		case WP_AIRSTRIKE:
+		case WP_SMOKE_BOMB:
 			break;
 
 		default:

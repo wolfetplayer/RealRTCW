d9f058068b4ed68fb8accfe62dff6dd1af0c02e9
German <karabastov@yahoo.com>
2026-05-01 16:47:26 +0300

    second pass
    its working very basically on openal

--- code/cgame/cg_view.c
+++ code/cgame/cg_view.c
@@ -1680,6 +1680,10 @@ void CG_DrawActiveFrame( int serverTime, stereoFrame_t stereoView, qboolean demo
 
 		DEBUGTIME
 
+		CG_AddScriptSpeakers();
+
+		DEBUGTIME
+
 		// Rafael particles
 		CG_AddParticles();
 		// done.
